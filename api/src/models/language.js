3bcfdb9be0e54ed19b248c83e53ef750b7506a7b
benndip <obentabiayuk1@gmail.com>
2023-02-03 12:43:50 +0100

    fix: redo changes code in the 'language.js' model file

--- api/src/models/language.js
+++ api/src/models/language.js
@@ -22,7 +22,7 @@ const CourseDetails = new mongoose.Schema({
 });
 
 const Course = new mongoose.Schema({
-  approved: { type: Boolean, required: false },
+  approved: { type: Boolean, required: true },
   admin_id: { type: String, required: true },
   details: { type: CourseDetails, required: true },
 });
